d0210f8fd0e25808e3be1b59950e288476cb70a1
moguchev <abracadabra.14@mail.ru>
2020-06-04 23:09:56 +0300

    tests was added

---
+++ K1804BC1 VS Project/dllmain.cpp
@@ -0,0 +1,28 @@
+﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
+#include "pch.h"
+#include "K1804BC1.h"
+
+#define model_key 0x00000000
+
+extern "C" IDSIMMODEL __declspec(dllexport)* createdsimmodel(CHAR* device, ILICENCESERVER* ils)
+{
+	if (ils->authorize(model_key))
+	{
+		return new K1804BC1;
+	}
+	else
+	{
+		MessageBox(0, L"Авторизация модели не прошла: [NULL]!", L"Внимание!", MB_ICONERROR);
+		return NULL;
+	}
+}
+
+extern "C" VOID __declspec(dllexport) deletedsimmodel(IDSIMMODEL* model)
+{
+	delete (K1804BC1*)model;
+}
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fwdreason, LPVOID lpvReserved)
+{
+	return 1;
+}
